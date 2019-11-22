8db31384d180aa2974c7c29e2aa13be015371992
puiiyuen <peiyuan.chen@smu.ca>
2019-11-22 12:35:17 -0400

    auto commit

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -26,27 +26,7 @@ module.exports = {
     sidebarDepth: 2,
     lastUpdated: 'Last Updated',
     repo: 'https://github.com/puiiyuen/course-registration-helper-backend',
-    repoLabel: 'GitHub',
-    docsRepo: 'https://github.com/puiiyuen/hci_docs',
-    // 假如文档不是放在仓库的根目录下：
-    // docsDir: 'docs',
-    // 假如文档放在一个特定的分支下：
-    // docsBranch: 'master',
-    // 默认是 false, 设置为 true 来启用
-    editLinks: true,
-    // 默认为 "Edit this page"
-    editLinkText: 'Edit page'
+    repoLabel: 'GitHub'
   },
-  plugins: [
-    'vuepress-plugin-table-of-contents'
-  // '@vuepress/last-updated',
-  // {
-  //   transformer: (timestamp, lang) => {
-  //     // 不要忘了安装 moment
-  //     const moment = require('moment')
-  //     moment.locale(lang)
-  //     return moment(timestamp).fromNow()
-  //   }
-  // }
-  ]
+  plugins: ['vuepress-plugin-table-of-contents']
 }
